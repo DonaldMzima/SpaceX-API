e35ed07338b4f02def7d11af0559bf6514e3f326
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2022-10-29 09:58:17 +0200

    feat/adding/dark-mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,13 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import SpaceX from '../components/spaceX'
 import { render } from 'react-dom'
+import { RecoilRoot } from 'recoil'
 
 const Home: NextPage = () => {
-
-
   return (
-   <SpaceX/>
+    <RecoilRoot>
+      <SpaceX />
+    </RecoilRoot>
   )
 }
 
