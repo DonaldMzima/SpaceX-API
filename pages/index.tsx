0131fdfb3f5f7e710f4c2552ca489848e727169d
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2022-12-17 12:41:54 +0200

    feat:adding/button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,4 @@
 import type { NextPage } from 'next'
-
-
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import SpaceX from '../components/spaceX'
-import { render } from 'react-dom'
-
-import Button from '../components/button'
-
 import { ChakraProvider } from '@chakra-ui/react'
 import ButtonX from '../components/ButtonX'
 
@@ -16,9 +6,6 @@ const Home: NextPage = () => {
   return (
     <ChakraProvider>
       <ButtonX />
-
-      <Button />
-
     </ChakraProvider>
   )
 }
