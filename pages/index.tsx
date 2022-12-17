96baa093843b53e13a14fa70d60c991150d429ce
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2022-12-17 10:27:32 +0200

    feat:removing/files

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,17 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import SpaceX from '../components/spaceX'
+
 import { render } from 'react-dom'
-import { RecoilRoot } from 'recoil'
+
 import Button from '../components/button'
+import { ChakraProvider } from '@chakra-ui/react'
 
 const Home: NextPage = () => {
   return (
-    <RecoilRoot>
+    <ChakraProvider>
       <Button />
-    </RecoilRoot>
+    </ChakraProvider>
   )
 }
 
