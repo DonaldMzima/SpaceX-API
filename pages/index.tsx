147c5e580aa10668070c7027fb5fce75cbb5a125
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2022-12-17 11:22:24 +0200

    ref:debugging

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,4 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-import { render } from 'react-dom'
 
 import Button from '../components/button'
 import { ChakraProvider } from '@chakra-ui/react'
--- store/atom.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from 'recoil'
-
-const darkModeState = atom<string | undefined | null>({
-  key: 'dark', // unique ID (with respect to other atoms/selectors)
-  default: undefined, // default value (aka initial value)
-})
-
-export { darkModeState }
