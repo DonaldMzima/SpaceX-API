6824493d5e769560d3ecc1e731996a8cc2639e28
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2022-12-18 21:33:09 +0200

    ref:adding:responsiveness

--- pages/spaceX.tsx
+++ pages/spaceX.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { Link } from '@chakra-ui/react'
+import { Box, Link } from '@chakra-ui/react'
 
 type RocketType = {
   rocket_id: string
@@ -64,58 +64,60 @@ const SpaceX = () => {
   }, [])
 
   return (
-    <div
-      style={{
-        textAlign: 'center',
-        flexDirection: 'column',
-        backgroundColor: '#597f85',
-        display: 'flex',
-        paintOrder: 'fixed',
-      }}
-    >
-      <h1
+    <Box mt={[2, 4, 6, 8]} width={[1, 1 / 2, 1 / 4]} height="24px">
+      <div
         style={{
-          boxShadow: '0px 5px 100px -7px rgba(29, 60, 55, 0.95)',
-          marginLeft: 420,
-          width: 500,
+          textAlign: 'center',
+          flexDirection: 'column',
+          backgroundColor: '#597f85',
+          display: 'flex',
           paintOrder: 'fixed',
         }}
       >
-        List of spaceX Launches:
-      </h1>
+        <h1
+          style={{
+            boxShadow: '0px 5px 100px -7px rgba(29, 60, 55, 0.95)',
+            marginLeft: 420,
+            width: 500,
+            paintOrder: 'fixed',
+          }}
+        >
+          List of spaceX Launches:
+        </h1>
 
-      {launches?.map((launch: LaunchType) => {
-        //  console.log ("checking",launch)
-        return (
-          <div
-            key={launch.launch_date_unix}
-            style={{
-              height: 550,
-              width: 500,
-              backgroundColor: '#ddedf0',
-              marginTop: 20,
-              marginLeft: 420,
-              borderRight: 'solid',
+        {launches?.map((launch: LaunchType) => {
+          //  console.log ("checking",launch)
+          return (
+            <div
+              key={launch.launch_date_unix}
+              style={{
+                height: 550,
+                width: 500,
+                backgroundColor: '#ddedf0',
+                marginTop: 20,
+                marginLeft: 420,
+                borderRight: 'solid',
 
-              borderLeft: 'solid',
-              borderRadius: 45,
-            }}
-          >
-            <img
-              alt={'Launch image'}
-              src={launch.links.mission_patch}
-              width={200}
-              height={180}
-            />
-            <p>flight nummber:{launch.flight_number}</p>-
-            <p>mission name:{launch.mission_name}</p>-
-            <p>rocket id:{launch.rocket.rocket_id}</p>-
-            <p>rocket name:{launch.rocket.rocket_name}</p>-
-            <p>rocket type:{launch.rocket.rocket_type}</p>
-          </div>
-        )
-      })}
-    </div>
+                borderLeft: 'solid',
+                borderRadius: 45,
+              }}
+            >
+              <img
+                alt={'Launch image'}
+                src={launch.links.mission_patch}
+                width={200}
+                height={180}
+              />
+              <p>flight nummber:{launch.flight_number}</p>-
+              <p>mission name:{launch.mission_name}</p>-
+              <p>rocket id:{launch.rocket.rocket_id}</p>-
+              <p>rocket name:{launch.rocket.rocket_name}</p>-
+              <p>rocket type:{launch.rocket.rocket_type}</p>
+            </div>
+          )
+        })}
+      </div>
+    </Box>
   )
 }
 
